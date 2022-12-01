4852adc250459896a1bd12dd48bc1eb7593667c4
Cupskrrt <khanalfarizzi@hotmail.com>
2022-12-02 05:55:20 +0700

    Creating light and dark mode button

--- src/components/ThemeMode.jsx
+++ src/components/ThemeMode.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SunIcon, MoonIcon} from '@heroicons/react/24/outline';
 
 const ThemeMode = () => {
+  const [icon, setIcon] = useState(false);
+
+  const element = document.documentElement;
+
+  useEffect(() => {
+    switch (icon) {
+      case true:
+        element.classList.add('dark');
+        break;
+      case false:
+        element.classList.remove('dark');
+        break;
+      default:
+        break;
+    }
+  }, [icon]);
+
   return (
     <>
       {/* TODO: Create theme mode using css */}
-      <div>
-        <SunIcon className="w-[2rem]" />
-        <MoonIcon className="w-[2rem]" />
-      </div>
+      <button className={`w-[2rem]`} onClick={() => setIcon(!icon)}>
+        {!icon && <SunIcon />}
+        {icon && <MoonIcon />}
+      </button>
     </>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  mode: 'jit',
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
