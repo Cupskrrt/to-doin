c908cd21f26183172d9a3245afd45b403741d3ad
Luthfi Khan <cupskrrtt@Luthfis-MacBook-Air.local>
2022-12-28 18:40:01 +0700

    [PATCH] minor fix

--- src/pages/TodayPage.jsx
+++ src/pages/TodayPage.jsx
@@ -7,7 +7,7 @@ const TodayPage = () => {
     <>
       <div className="flex flex-col items-center p-4 gap-5 w-[85vw] h-[90vh] overflow-y-auto dark:bg-gray-800 dark:text-white">
         <h1 className="font-bold">Today Page</h1>
-        <TaskCard query={getTodayTask} task="task" />;
+        <TaskCard query={getTodayTask} task="task" />
       </div>
     </>
   );
