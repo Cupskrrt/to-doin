8db96deed4c71b48746576fe4a99b97fd117a9d8
Luthfi Khan <cupskrrtt@Luthfis-MacBook-Air.local>
2022-12-30 21:40:04 +0700

    [TODO] fix this page to show only requested tag

--- src/pages/TagsPage.jsx
+++ src/pages/TagsPage.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { getTaskOngoing } from "../utils/api/taskApi.js";
 import TaskCard from "../components/TaskCard.jsx";
+import { getTaskbyTag } from "../utils/api/taskApi.js";
 import { useParams } from "react-router-dom";
 
 const TagsPage = () => {
   const { tagId } = useParams();
   return (
-    //TODO: FIX THIS PAGE CONTENT
+    //TODO: FIX TASK CARD FOR TAGSPAGE
     <>
       <div className="flex flex-col items-center p-4 gap-5 w-[85vw] h-[90vh] overflow-y-auto dark:bg-gray-800 dark:text-white">
         <h1 className="font-bold">{tagId}</h1>
-        <p>KONTOL</p>
-        {/* <TaskCard query={getTaskOngoing} task="task" /> */}
+        <TaskCard query={getTaskbyTag({ id: tagId })} task="taskTag" />
       </div>
     </>
   );
