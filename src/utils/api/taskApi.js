4b03fd0578c9ce62eb8b257036459cdc0584941a
Luthfi Khan <cupskrrtt@Luthfis-MacBook-Air.local>
2022-12-30 21:39:41 +0700

    [PATCH] remaking the getTaskByTag route

--- src/utils/api/taskApi.js
+++ src/utils/api/taskApi.js
@@ -46,18 +46,8 @@ export const getCountTaskToday = async () => {
   return res;
 };
 
-export const getTaskCompleted = async () => {
-  const res = await taskApi.get("/task/completed");
-  return res;
-};
-
-export const getTaskOngoing = async () => {
-  const res = await taskApi.get("/task/ongoing");
-  return res;
-};
-
-export const getTaskNotStarted = async () => {
-  const res = await taskApi.get("/task/not-started");
+export const getTaskbyTag = async ({ id }) => {
+  const res = await taskApi.get(`/task/${id}`);
   return res;
 };
 
