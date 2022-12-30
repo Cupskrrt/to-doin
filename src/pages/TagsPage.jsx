fbf57bb636cd1a6c1195d60f129560bea32d7724
Luthfi Khan <cupskrrtt@Luthfis-MacBook-Air.local>
2022-12-30 23:12:52 +0700

    [TODO] fix task card to show only the requested tag

--- src/pages/TagsPage.jsx
+++ src/pages/TagsPage.jsx
@@ -5,12 +5,13 @@ import { useParams } from "react-router-dom";
 
 const TagsPage = () => {
   const { tagId } = useParams();
+
   return (
-    //TODO: FIX TASK CARD FOR TAGSPAGE
+    //TODO: FIGURE OUT HOW TO MAKE THIS PAGE SHOWUP
     <>
       <div className="flex flex-col items-center p-4 gap-5 w-[85vw] h-[90vh] overflow-y-auto dark:bg-gray-800 dark:text-white">
         <h1 className="font-bold">{tagId}</h1>
-        <TaskCard query={getTaskbyTag({ id: tagId })} task="taskTag" />
+        <TaskCard task="taskTag" query={getTaskbyTag({ id: tagId })} />
       </div>
     </>
   );
