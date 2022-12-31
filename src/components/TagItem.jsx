6d07c62f135f19304de346b1908b9a10bc876597
Luthfi Khan <cupskrrtt@Luthfis-MacBook-Air.local>
2022-12-31 17:18:49 +0700

    [PATCH] adding new tag button (not final)

--- src/components/TagItem.jsx
+++ src/components/TagItem.jsx
@@ -1,22 +1,38 @@
-import React from "react";
-import { useQuery } from "react-query";
-import { getTag } from "../utils/api/tagApi.js";
+import React, { useState } from "react";
+import { getTagQuery } from "../utils/queries/tagQuery.js";
 import { Link } from "react-router-dom";
 import ColorConfig from "../utils/ColorProfile.js";
+import NewTag from "../components/NewTag.jsx";
 
 const TagItem = () => {
-  const { data } = useQuery("tag", getTag);
+  const [newTag, setNewTag] = useState(false);
+  const { data } = getTagQuery();
+
+  const isHidden = () => {
+    setNewTag(!newTag);
+  };
+
   return (
     <>
       {data?.data.map((tag) => {
         return (
-          <div key={tag.title} className={ColorConfig[`${tag.color}`]}>
+          <div key={tag._id} className="ml-4">
             <Link to={`/tag/${tag._id}`} className="text-xs">
-              {tag.title}
+              {tag.name}
             </Link>
           </div>
         );
       })}
+      {newTag ? (
+        <NewTag />
+      ) : (
+        <p
+          onClick={isHidden}
+          className="sticky bottom-0 hover:cursor-pointer border-2 rounded-xl w-[7vw] text-center p-1 bg-white text-xs"
+        >
+          Add New Tag +
+        </p>
+      )}
     </>
   );
 };
