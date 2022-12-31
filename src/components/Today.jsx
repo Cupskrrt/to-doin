1b99dbaefa9b6d44f7f36a9fdc545f3f4feed6df
Luthfi Khan <cupskrrtt@Luthfis-MacBook-Air.local>
2022-12-31 17:19:36 +0700

    [PATCH] added the new query hook

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { BellIcon } from "@heroicons/react/24/outline";
-import { useQuery, useQueryClient } from "react-query";
-import { getCountTaskToday } from "../utils/api/taskApi";
+import { useQueryClient } from "react-query";
+import { getCountTaskTodayQuery } from "../utils/queries/taskQuery.js";
 
 const Today = () => {
-  const { data } = useQuery("countToday", getCountTaskToday);
-  useQueryClient().invalidateQueries("countToday");
+  const { data } = getCountTaskTodayQuery();
+  useQueryClient().invalidateQueries("count");
   return (
     <div className="flex justify-between items-center">
       <div className="flex gap-4">
