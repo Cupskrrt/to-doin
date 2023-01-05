682b071fde82c48b5db44c2e468893853923250b
Luthfi Khan <cupskrrtt@Luthfis-MacBook-Air.local>
2023-01-05 08:10:21 +0700

    [PATCH] change the code to use new query

--- src/pages/ImportantPage.jsx
+++ src/pages/ImportantPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { getImportantTaskQuery } from "../utils/queries/taskQuery.js";
-import TaskCard from "../components/TaskCard.jsx";
+import Task from "../components/Task.jsx";
 
 const ImportantPage = () => {
   return (
     <>
       <div className="flex flex-col items-center p-4 gap-5 w-[85vw] h-[90vh]">
         <h1 className="font-bold">Important</h1>
-        <TaskCard query={getImportantTaskQuery()} />
+        <Task query={getImportantTaskQuery()} />
       </div>
     </>
   );
--- src/pages/TodayPage.jsx
+++ src/pages/TodayPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { getTodayTaskQuery } from "../utils/queries/taskQuery.js";
-import TaskCard from "../components/TaskCard";
+import Task from "../components/Task.jsx";
 
 const TodayPage = () => {
   return (
     <>
       <div className="flex flex-col items-center p-4 gap-5 w-[85vw] h-[90vh] overflow-y-auto dark:bg-gray-800 dark:text-white">
         <h1 className="font-bold">Today Page</h1>
-        <TaskCard query={getTodayTaskQuery()} />
+        <Task query={getTodayTaskQuery()} />
       </div>
     </>
   );
