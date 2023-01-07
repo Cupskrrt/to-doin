6c7b10c96e9a951024ec18c5f4050b472a8dcbd4
Luthfi Khan <cupskrrtt@Luthfis-MacBook-Air.local>
2023-01-07 23:47:27 +0700

    [PATCH] minor change

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div className="flex justify-between p-4 items-center border-b-2 h-[8vh]">
         <h1 className="font-bold text-2xl">to-doin</h1>
         <div className="flex gap-5">
-          <BellIcon className="w-[1.5rem]" onClick={(e) => setNotif(!notif)} />
+          <BellIcon className="w-[1.5rem]" onClick={() => setNotif(!notif)} />
           {notif ? <Notification /> : null}
           <Profile />
         </div>
--- src/pages/TagsPage.jsx
+++ src/pages/TagsPage.jsx
@@ -9,7 +9,7 @@ const TagsPage = () => {
   return (
     <>
       <div className="flex flex-col items-center p-4 gap-5 w-[85vw] h-[90vh] overflow-y-auto dark:bg-gray-800 dark:text-white">
-        <h1 className="font-bold">{tagId}</h1>
+        <h1 className="font-bold">{}</h1>
         <TaskCard query={getTaskbyTagQuery(tagId)} />
       </div>
     </>
